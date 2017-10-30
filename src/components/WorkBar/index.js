21fd2e17b2649b978c8c97234a44a23114a502ae
awarn <anton@rebelandbird.com>
2017-10-30 22:04:56 +0100

    WIP WorkBar

---
+++ src/components/WorkBar/index.js
@@ -0,0 +1,61 @@
+
+import React from "react"
+
+import s from "./style.scss"
+
+export default class Navigation extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			target: 150,
+			workShares: [
+				{
+					name: "Linda",
+					amount: 44.1	
+				},
+				{
+					name: "Jim",
+					amount: 67.13	
+				},
+				{
+					name: "Ditt bidrag",
+					amount: 78.33
+				}
+			]
+		}
+	}
+
+	render() {
+		let target = this.state.target
+		let targetOverflow = target * 1.25
+
+		let total = this.state.workShares.reduce((total, current) => {
+			return total + current.amount
+		}, 0)
+
+		let sections = this.state.workShares.map((share, i) => {
+			let style = {
+				width: `${100 * share.amount / total}%`
+			}
+			
+			return (
+				<div key={`share-${i}`} className={s.share} style={style}>{share.name}</div>
+			)
+		})
+
+		return (
+			<div className={s.wrapper}>
+				<div className={s.bar}>
+					<div className={s.total} style={{width: `${100 * total / target}%`}}>
+						{sections}
+					</div>
+					<div className={s.targetMarker}>
+						<span className={s.targetMarkerText}>{this.state.target}</span>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
--- src/routes/work/index.js
+++ src/routes/work/index.js
@@ -1,6 +1,7 @@
 
 import React from "react"
 
+import WorkBar from "./../../components/WorkBar"
 import WorkPerformanceValue from "./../../components/WorkPerformanceValue"
 
 export default class Work extends React.Component {
@@ -13,7 +14,16 @@ export default class Work extends React.Component {
 			<div>
 				<div className="container">
 					<div className="col-sm-6">
-						<h1>Work</h1>
+						<h1>Jobb</h1>
+					</div>
+				</div>
+				<div className="container">
+					<div className="col-sm-6">
+						<h2>Ditt team</h2>
+						<WorkBar/>
+					</div>
+					<div className="col-sm-6">
+						<h2>Din dag</h2>
 					</div>
 				</div>
 				<div className="container">
