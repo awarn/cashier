0676bc1d235a01fb8f27d0bb776a10a0a51fa33a
awarn <anton@rebelandbird.com>
2018-02-17 20:57:29 +0100

    Revert "Add login to user page"
    This reverts commit 20735f20ee90a2944c5f3992d2ba7999a3de9bf3.

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -13,38 +13,20 @@ export default class Work extends React.Component {
 		super()
 	}
 
-	login() {
-		auth.login();
-	}
-
 	render() {
-		const { isAuthenticated } = auth;
-
 		return (
 			<div>
-			{
-				!isAuthenticated() &&
-					<div>
-						<h1>Log in using Auth0</h1>
-						<button
-							className="form-control"
-							onClick={this.login.bind(this)}>Log In</button>
+				<div className="container">
+					<div className="col-sm-6">
+						<Login/>
 					</div>
-			}
-			{
-				isAuthenticated() && 
-					<div className="container">
-						<div className="col-sm-6">
-							<Login/>
-						</div>
-						<div className="col-sm-6">
-							<TeamList />
-						</div>
-						<div className="col-sm-6">
-							<Profile auth={auth}/>
-						</div>
+					<div className="col-sm-6">
+						<TeamList />
 					</div>
-			}
+					<div className="col-sm-6">
+						<Profile auth={auth}/>
+					</div>
+				</div>
 			</div>
 		)
 	}
