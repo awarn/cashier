ba2ae340128e5a995b1b6e7ff7b6780dd1d563fa
awarn <anton@rebelandbird.com>
2018-02-26 15:57:06 +0100

    Add material design icons

--- src/middleware.js
+++ src/middleware.js
@@ -14,6 +14,7 @@ export default (req, res) => {
 					<title>Cashier</title>
 					<meta charset="utf-8">
 					<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+					<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 				</head>
 				<body>
@@ -33,6 +34,7 @@ export default (req, res) => {
 				<head>
 					<title>Cashier</title>
 					<!--<link rel="stylesheet" href="bundle.css">-->
+					<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 				</head>
 				<body>
 					<div id="root">${renderToString(
