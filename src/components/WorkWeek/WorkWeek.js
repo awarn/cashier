779cf53348f378383f8a95636b95cdc486814e8d
awarn <anton@rebelandbird.com>
2018-03-04 15:37:56 +0100

    Add view switches to work week

--- src/components/WorkWeek/WorkWeek.js
+++ src/components/WorkWeek/WorkWeek.js
@@ -8,47 +8,129 @@ export default class WorkWeek extends React.Component {
 		super(props);
 
 		this.state = {
-			profile: props.profile
+			profile: props.profile,
+			thisWeekView: "total",
+			lastWeekView: "total"
 		}
 	}
 
+	async setThisWeekView(view) {
+		let ok = false;
+		switch (view) {
+			case "total":
+				ok = true;
+				break;
+			case "projection":
+				ok = true;
+				break;
+		}
+		if (ok) {
+			this.setState({
+				thisWeekView: view
+			})
+		}	
+	}
+
+	async setLastWeekView(view) {
+		let ok = false;
+		switch (view) {
+			case "total":
+				ok = true;
+				break;
+			case "timepoint":
+				ok = true;
+				break;
+		}
+		if (ok) {
+			this.setState({
+				lastWeekView: view
+			})
+		}	
+	}
+
 	render() {
-		const { profile } = this.state;
+		const { profile, thisWeekView, lastWeekView } = this.state;
 
 		return (
 			<div className={s.workWeek}>
-				<div className={s.label}>
-					Totalt:
-				</div>
 
-				<div className={s.valueFrame}>
-					{
-						profile &&
-							<WorkPerformanceValue url={`/user/work/totalThisWeek?user=${profile.name}`} unit="kr"/>
-					}
-				</div>
+				<div className={s.thisWeek}>
+					<div className={s.label}>
+						Totalt:
+					</div>
 
-				<div className={s.label}>
-					Förra veckan:
-				</div>
-
-				<div className={s.oneWeekAgo}>
 					<div className={s.valueFrame}>
 						{
-							profile &&
+							profile && thisWeekView === "total" &&
+								<WorkPerformanceValue url={`/user/work/totalThisWeek?user=${profile.name}`} unit="kr"/>
+						}
+						{
+							profile && thisWeekView === "projection" &&
 								<WorkPerformanceValue url={`/user/work/totalLastWeek?user=${profile.name}`} unit="kr"/>
 						}
 					</div>
 
 					<div className={s.comparisons}>
-						<button className={s.comparisonValue}>
-							Samma
-							<i className="material-icons">face</i>
-						</button>
-						<button className={s.comparisonValue}>
-							Totalt
-							<i className="material-icons">star</i>
-						</button>
+						<div className="btn-group btn-group-toggle" data-toggle="buttons">
+							<label 
+								onClick={this.setThisWeekView.bind(this, "total")} 
+								className={`btn btn-secondary ${thisWeekView === "total" ? "active" : ""}`}>
+								<input defaultChecked
+									type="radio" 
+									name="thisWeekView" 
+									autoComplete="off" />Totalt
+							</label>
+
+							<label 
+								onClick={this.setThisWeekView.bind(this, "projection")} 
+								className={`btn btn-secondary ${thisWeekView === "projection" ? "active" : ""}`}>
+								<input 
+									type="radio" 
+									name="thisWeekView" 
+									autoComplete="off" />Projektion
+							</label>
+						</div>
+					</div>
+				</div>
+
+				<hr/>
+
+				<div className={s.oneWeekAgo}>
+					<div className={s.label}>
+						Förra veckan:
+					</div>
+
+					<div className={s.valueFrame}>
+						{
+							profile && lastWeekView === "total" &&
+								<WorkPerformanceValue url={`/user/work/totalLastWeek?user=${profile.name}`} unit="kr"/>
+						}
+						{
+							profile && lastWeekView === "timepoint" &&
+								<WorkPerformanceValue url={`/user/work/totalThisWeek?user=${profile.name}`} unit="kr"/>
+						}
+					</div>
+
+					<div className={s.comparisons}>
+						<div className="btn-group btn-group-toggle" data-toggle="buttons">
+							<label 
+								onClick={this.setLastWeekView.bind(this, "total")} 
+								className={`btn btn-secondary ${lastWeekView === "total" ? "active" : ""}`}>
+								<input defaultChecked
+									type="radio" 
+									name="lastWeekView" 
+									autoComplete="off" />Totalt
+							</label>
+
+							<label 
+								onClick={this.setLastWeekView.bind(this, "timepoint")} 
+								className={`btn btn-secondary ${lastWeekView === "timepoint" ? "active" : ""}`}>
+								<input 
+									type="radio" 
+									name="lastWeekView" 
+									autoComplete="off" />Samma tid
+							</label>
+						</div>
 					</div>
 				</div>
 			</div>
